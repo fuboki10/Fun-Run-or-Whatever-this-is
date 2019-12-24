2807bbc222900ff0aa1e4ac0b4b6a45da03c6f64
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-12-24 05:20:18 +0200

    Obstacle 1

--- src/common/obstacle.ts
+++ src/common/obstacle.ts
@@ -27,8 +27,8 @@ export class Obstacle
                 ambient_occlusion: textures['white']},
                 modelMatrix: mat4.create(),
                 aabb : new AABB(MeshUtils.Cube(gl)),    
-                physics:new physics(vec3.fromValues(5, 0, zCord), // we will change velocity later
-                vec3.fromValues(0.01, 0, 0), true, vec3.fromValues(0, 0, 0))
+                physics:new physics(vec3.fromValues(-4, 0, zCord), // we will change velocity later
+                vec3.fromValues(0.005, 0, 0), true, vec3.fromValues(0, 0, 0))
             };
             this.Objects[1]={
                 mesh:MeshUtils.Cube(gl),
@@ -43,11 +43,11 @@ export class Obstacle
                 ambient_occlusion: textures['white']},
                 modelMatrix: mat4.create(),
                 aabb : new AABB(MeshUtils.Cube(gl)),
-                physics:new physics(vec3.fromValues(5, 0, zCord+1), // we will change velocity later
-                vec3.fromValues(0.01/3, 0, 0), true, vec3.fromValues(0, 0, 0))
+                physics:new physics(vec3.fromValues(-3, 0, zCord+1), // we will change velocity later
+                vec3.fromValues(0.005, 0, 0), true, vec3.fromValues(0, 0, 0))
             }
             this.Objects[2]={
-                mesh:MeshUtils.Cube(gl),
+                mesh:MeshUtils.Cube(gl),    
                 material: {albedo: textures['snow.albedo'],
                 albedo_tint: vec3.fromValues(1, 1, 1),
                 specular: textures['snow.specular'],
@@ -59,8 +59,8 @@ export class Obstacle
                 ambient_occlusion: textures['white']},
                 modelMatrix: mat4.create(),
                 aabb : new AABB(MeshUtils.Cube(gl)),
-                physics:new physics(vec3.fromValues(5, 0, zCord+2), // we will change velocity later
-                vec3.fromValues(0.01/6, 0, 0), true, vec3.fromValues(0, 0, 0))
+                physics:new physics(vec3.fromValues(-2, 0, zCord+2), // we will change velocity later
+                vec3.fromValues(0.005, 0, 0), true, vec3.fromValues(0, 0, 0))
             };
         }
         if (rand == 2)
