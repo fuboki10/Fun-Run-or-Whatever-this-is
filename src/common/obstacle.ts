d028aea4ea185084b4a1fae767c16f7e2549d576
alyabozied <42724850+alyabozied@users.noreply.github.com>
2019-12-24 06:31:26 +0200

    putting all obstacles

--- src/common/obstacle.ts
+++ src/common/obstacle.ts
@@ -27,11 +27,7 @@ export class Obstacle
                 ambient_occlusion: textures['white']},
                 modelMatrix: mat4.create(),
                 aabb : new AABB(MeshUtils.Cube(gl)),    
-<<<<<<< HEAD
-                physics:new physics(vec3.fromValues(5, 0, zCord), // we will change velocity later
-=======
                 physics:new physics(vec3.fromValues(-4, 0, zCord), // we will change velocity later
->>>>>>> 2807bbc222900ff0aa1e4ac0b4b6a45da03c6f64
                 vec3.fromValues(0.005, 0, 0), true, vec3.fromValues(0, 0, 0))
             };
             this.Objects[1]={
@@ -47,13 +43,8 @@ export class Obstacle
                 ambient_occlusion: textures['white']},
                 modelMatrix: mat4.create(),
                 aabb : new AABB(MeshUtils.Cube(gl)),
-<<<<<<< HEAD
-                physics:new physics(vec3.fromValues(5, 0, zCord+1), // we will change velocity later
-                vec3.fromValues(0.005/3, 0, 0), true, vec3.fromValues(0, 0, 0))
-=======
                 physics:new physics(vec3.fromValues(-3, 0, zCord+1), // we will change velocity later
                 vec3.fromValues(0.005, 0, 0), true, vec3.fromValues(0, 0, 0))
->>>>>>> 2807bbc222900ff0aa1e4ac0b4b6a45da03c6f64
             }
             this.Objects[2]={
                 mesh:MeshUtils.Cube(gl),    
@@ -68,13 +59,8 @@ export class Obstacle
                 ambient_occlusion: textures['white']},
                 modelMatrix: mat4.create(),
                 aabb : new AABB(MeshUtils.Cube(gl)),
-<<<<<<< HEAD
-                physics:new physics(vec3.fromValues(5, 0, zCord+2), // we will change velocity later
-                vec3.fromValues(0.005/6, 0, 0), true, vec3.fromValues(0, 0, 0))
-=======
                 physics:new physics(vec3.fromValues(-2, 0, zCord+2), // we will change velocity later
                 vec3.fromValues(0.005, 0, 0), true, vec3.fromValues(0, 0, 0))
->>>>>>> 2807bbc222900ff0aa1e4ac0b4b6a45da03c6f64
             };
         }
         if (rand == 2)
@@ -198,6 +184,7 @@ export class Obstacle
                 vec3.fromValues(0, -0.001, 0), true, vec3.fromValues(0, 0, 0))
             };
         }
+        console.log(this.Objects[0]);
     }
     public Update(deltaTime:number)
     {
